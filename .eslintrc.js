5cc4640468da8e3c9c7257eee990b308e34733e9
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-03-02 20:29:13 -0300

    Configurando estrutura

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     'react',
     'prettier',
+    'react-hooks'
   ],
   rules: {
     "prettier/prettier":["error", {"singleQuote": true}],
@@ -37,6 +38,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement':'off',
-    'no-console': ['error', {allow: ['tron']}]
+    'no-console': ['error', {allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
